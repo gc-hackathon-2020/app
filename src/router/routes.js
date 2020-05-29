d5a6317bd9dec6711bcd6f91f3fdfde56cb9a523
Mykola <mkrusser@goldencomm.com>
2020-05-29 18:59:05 +0200

    US05 | Friends List with static data

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: 'settings', component: () => import('pages/AccountSettings.vue') }
+      { path: 'settings', component: () => import('pages/AccountSettings.vue') },
+      { path: 'friends', component: () => import('pages/Friendslist.vue') },
     ]
   }
 ];
